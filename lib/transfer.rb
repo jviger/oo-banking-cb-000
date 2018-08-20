bd4d9265f646e0b6fe58fe63e12efab77b253091
Jason Viger <jason.viger@gmail.com>
2018-08-20 10:36:24 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,6 +23,7 @@ class Transfer
     if self.valid?
       receiver.deposit(amount)
       sender.balance -= amount
+      status = "complete"
     else 
       "Transaction rejected. Please check your account balance."
     end
